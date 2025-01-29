0f22524cd34ba662b7e3b37337963074bdb52302
Karan Kumar <officialkaran39@gmail.com>
2025-01-29 18:30:20 +0530

    Social media system problem completed

---
+++ Day01/LinkedList/singlylinkedlist/socialmediaconnection/Main.java
@@ -0,0 +1,29 @@
+package singlylinkedlist.socialmediaconnection;
+
+public class Main {
+    public static void main(String[] args) {
+        SocialMediaSystem system = new SocialMediaSystem();
+
+        // Add users
+        system.addUser(1, "Karan", 25);
+        system.addUser(2, "Ashish", 28);
+        system.addUser(3, "Priyanshu", 22);
+
+        // Add friends
+        system.addFriend(1, 2);
+        system.addFriend(1, 3);
+        system.addFriend(2, 3);
+
+        // Display friends
+        system.displayFriends(1);
+        system.displayFriends(2);
+
+        // Find mutual friends
+        system.findMutualFriend(1, 2);
+
+        // Remove a friend connection
+
+        // Count friends
+        system.countFriends(1);
+    }
+}
