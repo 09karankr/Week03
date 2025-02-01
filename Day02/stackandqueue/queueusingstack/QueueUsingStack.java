a804323366d27edef2e49068e3b6172b815fc149
Karan Kumar <officialkaran39@gmail.com>
2025-02-01 22:44:03 +0530

    Completed the problem Queue using stack

--- Day02/stackandqueue/queueusingstack/QueueUsingStack.java
+++ Day02/stackandqueue/queueusingstack/QueueUsingStack.java
@@ -1,48 +1,48 @@
-package queueusingstack;
+    package queueusingstack;
 
 
 
-public class QueueUsingStack {
-    private StackUsingLinkedList stack1;
-    private StackUsingLinkedList stack2;
+    public class QueueUsingStack {
+        private StackUsingLinkedList stack1;
+        private StackUsingLinkedList stack2;
 
-    public QueueUsingStack() {
-        stack1 = new StackUsingLinkedList();
-        stack2 = new StackUsingLinkedList();
-    }
-
-    // enqueue operation
-    public void enqueue(int value){
-        stack1.push(value);
-        System.out.println(value + "Enqueue");
-    }
-    // dequeue operation
-    public int dequeue(){
-        if (isEmpty()){
-            System.out.println("Queue is empty");
+        public QueueUsingStack() {
+            stack1 = new StackUsingLinkedList();
+            stack2 = new StackUsingLinkedList();
         }
-       if (stack2.isEmpty()){
-           // Transfer elements from stack 1 to stack 2
-           while(!stack1.isEmpty()){
-               stack2.push(stack1.pop());
-           }
-       }
-       return stack2.pop();
-    }
 
-    public int peek(){
-        if (stack2.isEmpty()){
-            throw new RuntimeException("Queue is empty");
+        // enqueue operation
+        public void enqueue(int value){
+            stack1.push(value);
+            System.out.println(value + "Enqueue");
         }
-        if(stack2.isEmpty()){
-            while(!stack1.isEmpty()){
-                stack2.push(stack1.pop());
+        // dequeue operation
+        public int dequeue(){
+            if (isEmpty()){
+                System.out.println("Queue is empty");
             }
+           if (stack2.isEmpty()){
+               // Transfer elements from stack 1 to stack 2
+               while(!stack1.isEmpty()){
+                   stack2.push(stack1.pop());
+               }
+           }
+           return stack2.pop();
         }
-        return stack2.peek();
-    }
 
-    public boolean isEmpty(){
-        return stack1.isEmpty() && stack2.isEmpty();
+        public int peek(){
+            if (stack2.isEmpty()){
+                throw new RuntimeException("Queue is empty");
+            }
+            if(stack2.isEmpty()){
+                while(!stack1.isEmpty()){
+                    stack2.push(stack1.pop());
+                }
+            }
+            return stack2.peek();
+        }
+
+        public boolean isEmpty(){
+            return stack1.isEmpty() && stack2.isEmpty();
+        }
     }
-}
