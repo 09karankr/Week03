d0ac208ba1fa95c21e2330c415df3a7243663a9b
Karan Kumar <officialkaran39@gmail.com>
2025-02-05 15:25:03 +0530

    [Karan Kumar]Add. concatenate strings program completed

---
+++ Day04/src/main/java/stringbuffer/concatenatestring/Main.java
@@ -0,0 +1,9 @@
+package stringbuffer.concatenatestring;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        String[] words = {"Hello"," ","world", "!"," This"," is"," Java","."};
+        System.out.println("Concatenated string: " + ConcatenateStrings.concatenateStrings(words));
+    }
+}
