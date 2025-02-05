f6d3b653a575d705befdcfb255c329baa94ad87e
Karan Kumar <officialkaran39@gmail.com>
2025-02-05 13:23:21 +0530

    [Karan Kumar]Add. remove duplicates from a string program completed

---
+++ Day04/src/main/java/stringbuilder/removeduplicates/Main.java
@@ -0,0 +1,9 @@
+package stringbuilder.removeduplicates;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        String str = "programming";
+        System.out.println("String without duplicates: " + RemoveDuplicates.removeDuplicates(str));
+    }
+}
