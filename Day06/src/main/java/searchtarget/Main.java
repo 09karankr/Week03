b6e16bbab4b8af5753a43d7a8b420a146fc7623a
Karan Kumar <officialkaran39@gmail.com>
2025-02-08 11:44:31 +0530

    [Karan Kumar]Add. Added the program for Compare the performance of Linear Search (O(N)) and Binary Search (O(log N)) on different dataset sizes.

---
+++ Day06/src/main/java/searchtarget/Main.java
@@ -0,0 +1,9 @@
+package searchtarget;
+
+public class Main {
+    public static void main(String[] args) {
+     SearchTargetValue.CompareTime(1000);
+     SearchTargetValue.CompareTime(10000);
+     SearchTargetValue.CompareTime(1000000);
+    }
+}
