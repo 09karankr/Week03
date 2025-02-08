d03f18ae4e97407457f2f913c7024939628c8e6a
Karan Kumar <officialkaran39@gmail.com>
2025-02-08 12:40:04 +0530

    [Karan Kumar]Add. Added the program for Compare sorting algorithms Bubble Sort (O(N²)), Merge Sort (O(N log N)), and Quick Sort (O(N log N)).

---
+++ Day06/src/main/java/sortlargedata/Main.java
@@ -0,0 +1,9 @@
+package sortlargedata;
+
+public class Main {
+    public static void main(String[] args) {
+        SortingAlgoComparison.CompareTime(1000);
+        SortingAlgoComparison.CompareTime(10000);
+        SortingAlgoComparison.CompareTime(1000000);
+    }
+}
